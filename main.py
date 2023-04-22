0ba29ea75f8fd07629d08d8fe0e5114036dbc2c6
Jerry Chu <mlgerrydoge@gmail.com>
2023-04-22 14:08:30 -0700

    add html stuff

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return "Hello World"
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+@app.route("/result", methods = ['POST', 'GET'])
+def result():
+    output = request.form.to_dict()
+    name = output["name"]
+
+    return render_template("index.html", name = name);
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
