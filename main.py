a77b831244abfb89bed68c9392029beb155ae5d1
Jerry Chu <mlgerrydoge@gmail.com>
2023-04-22 12:29:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from flask import Flask
 
+app = Flask(__name__)
 
-class App(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.title = 'My App'
-        self.left = 100
-        self.top = 100
-        self.width = 640
-        self.height = 480
-        self.initUI()
+@app.route("/")
+def main():
+    return "Hello World"
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-
-        # Add a label for the header
-        header_label = QLabel('My App Header', self)
-        header_label.move(10, 10)
-
-        self.show()
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=80)
