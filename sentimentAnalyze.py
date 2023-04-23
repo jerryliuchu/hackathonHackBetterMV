07c3deb753fc9176c03cf1cb43e0c61d638841a0
Jerry Chu <mlgerrydoge@gmail.com>
2023-04-22 17:27:06 -0700

    f

--- sentimentAnalyze.py
+++ sentimentAnalyze.py
@@ -18,7 +18,7 @@ def sentiment_scores(sentence):
     ne = sentiment_dict['neu'] * 100
     p = sentiment_dict['pos'] * 100
 
-    x = "Your journal is " + n + "% negative, " + ne + "% neutral, and " + p + "% positive, and overall, your journal is "
+    x = "Your journal is " + str(round(n, 1)) + "% negative, " + str(round(ne, 1)) + "% neutral, and " + str(round(p,1)) + "% positive, and overall, your journal is "
 
     # decide sentiment as positive, negative and neutral
     if sentiment_dict['compound'] >= 0.05:
